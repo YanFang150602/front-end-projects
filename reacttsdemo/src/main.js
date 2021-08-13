52d914e7ed1fdcd5139ae2a10e27fd8b2a494650
颜泽芳 <838641570@qq.com>
2021-08-13 09:33:26 +0800

    Create main.js

---
+++ reacttsdemo/src/main.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import _ from 'lodash';
+console.log('I am main.js');
+/* $(document).ready(function() {
+  console.log('hello !');
+}); */
+_.each([1, 2], (item) => {
+  console.log(item);
+});
