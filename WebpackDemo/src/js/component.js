f0da6dc5d2accf12eea91953c890ca7965d3fd00
颜泽芳 <838641570@qq.com>
2021-09-14 15:08:06 +0800

    Create component.js

---
+++ WebpackDemo/src/js/component.js
@@ -0,0 +1,14 @@
+export const testJoin = function () {
+  const arr = [];
+  for (let i = 0; i < 10; i++) {
+    arr.push(i);
+  }
+  console.log(arr.join('=========='));
+}
+export const testReg = function () {
+  const arr = [];
+  for (let i = 0; i < 10; i++) {
+    arr.push(i);
+  }
+  console.log(arr.join('=========='));
+}
