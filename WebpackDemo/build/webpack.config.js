9e9415eec516aabadc634ac2f28746932e2cd44d
颜泽芳 <838641570@qq.com>
2021-09-15 17:17:36 +0800

    Update webpack.config.js

--- WebpackDemo/build/webpack.config.js
+++ WebpackDemo/build/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const webpackConfig = {
   entry: {
@@ -43,7 +43,7 @@ webpackConfig.plugins.push(
   })
 );
 webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
-webpackConfig.plugins.push(new BundleAnalyzerPlugin());
+// webpackConfig.plugins.push(new BundleAnalyzerPlugin());
 // webpackConfig.devtool = 'source-map';
 webpackConfig.devServer = {
   static: './dist',
