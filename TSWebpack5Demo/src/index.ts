15012c5b419f35132b0b9e73d6f7bf2fe4fdf1e0
颜泽芳 <838641570@qq.com>
2023-09-27 14:22:24 +0800

    Update index.ts

--- TSWebpack5Demo/src/index.ts
+++ TSWebpack5Demo/src/index.ts
@@ -1,2 +1,8 @@
-let s: string = 'test';
+import Home from './component/Home';
+
+let s:string = 'test';
 console.log(s);
+
+const home = new Home('谨防诈骗！！！');
+console.log(home.ads);
+home.changeAdsContent('诈骗防不胜防，多长个心眼！！！');
