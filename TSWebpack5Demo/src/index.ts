b123a36ad5559aaf9940b296ea8a0392602c5a29
颜泽芳 <838641570@qq.com>
2023-09-27 14:56:19 +0800

    Update index.ts

--- TSWebpack5Demo/src/index.ts
+++ TSWebpack5Demo/src/index.ts
@@ -1,12 +1,10 @@
 import Home from './component/Class';
 import Enum from './component/Enum';
 
-
-let s:string = 'test';
-console.log(s);
-
+console.log('------------------------class');
 const home = new Home('谨防诈骗！！！');
 console.log(home.ads);
 home.changeAdsContent('诈骗防不胜防，多长个心眼！！！');
 
+console.log('\n\n------------------------enum');
 console.log(Enum);
